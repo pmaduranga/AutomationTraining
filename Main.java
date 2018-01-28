158c2f34a8d7dd472971465ba9bece578f5f3715
Prasad Maduranga <35583135+pmaduranga@users.noreply.github.com>
2018-01-28 20:36:16 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+package selenium.automation.assignment4;
+
+public class Main {
+
+	/**
+	 * @author: Prasad
+	 */
+	public static void main(String[] args) {
+
+		TabulatorInfo obj = new TabulatorInfo();
+
+		obj.executeTabulatorInfo();
+
+	}
+
+}
